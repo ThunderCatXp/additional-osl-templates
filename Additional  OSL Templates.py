485386fa9b2f4f0fb06e40f7aa2788184b8e0b64
ThunderCat <thundercatxp@yahoo.com>
2023-02-25 23:46:48 +0300

    Add files via upload

---
+++ Additional  OSL Templates.py
@@ -0,0 +1,59 @@
+#  Original code source from https://blender.stackexchange.com/questions/1704/how-to-add-a-new-text-editor-template
+#  Thanks to https://github.com/sambler/osl-shaders and https://github.com/redshift3d/RedshiftOSLShaders#readme for
+#  collections of OSL shaders.
+
+import bpy
+bl_info = {
+    "name": "Additional OSL Templates",
+    "author": "Thunder Cat",
+    "location": "Scripting > Templates",
+    "version": (1, 0),
+    "blender": (3, 4, 0),
+    "category": "Text Editor"
+}
+
+
+class TEXT_MT_templates_redshiftOSL(bpy.types.Menu):
+    bl_label = "RedshiftOSL"
+    bl_idname = "OBJECT_MT_custom_Template1_menu"
+
+    def draw(self, context):
+        self.path_menu(
+            bpy.utils.script_paths(subdir=("templates_redshiftOSL")),
+            "text.open",
+            props_default={"internal": True},
+        )
+
+
+class TEXT_MT_templates_samblerOSL(bpy.types.Menu):
+    bl_label = "SamblerOSL"
+    bl_idname = "OBJECT_MT_custom_Template2_menu"
+
+    def draw(self, context):
+        self.path_menu(
+            bpy.utils.script_paths(subdir=("templates_samblerOSL")),
+            "text.open",
+            props_default={"internal": True},
+        )
+
+
+def draw_item(self, context):
+    layout = self.layout
+    layout.menu(TEXT_MT_templates_redshiftOSL.bl_idname)
+    layout.menu(TEXT_MT_templates_samblerOSL.bl_idname)
+
+
+def register():
+    bpy.utils.register_class(TEXT_MT_templates_samblerOSL)
+    bpy.utils.register_class(TEXT_MT_templates_redshiftOSL)
+    bpy.types.TEXT_MT_templates.append(draw_item)
+
+
+def unregister():
+    bpy.utils.unregister_class(TEXT_MT_templates_samblerOSL)
+    bpy.utils.unregister_class(TEXT_MT_templates_redshiftOSL)
+    bpy.types.TEXT_MT_templates.remove(draw_item)
+
+
+if __name__ == "__main__":
+    register()
---
+++ README.md
@@ -0,0 +1,13 @@
+## Shader source links: ##
+
+[RedshiftOSLShaders](https://github.com/redshift3d/RedshiftOSLShaders)
+
+[SamblerOSLShaders](https://github.com/sambler/osl-shaders)
+
+## INSTALATION GUIDE: ##
+
+1.Copy **templates_redshiftOSL** and **templates_samblerOSL** in **[version_number]/scripts** subfolder of your Blender installation, or in any additional path(s) for scripts you've set in User Preferences.
+
+2.In Blender **Edit>Preferences>Add-ons** install **Additional  OSL Templates.py**.
+
+3.To use new templates go to **Scripting** in Blender and press **Templates** button in **Text Editor Menu**
